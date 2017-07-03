7fd8925c3b0a80d85c1966a6ccbd447be227cf0d
Brice Colucci <brice.colucci@gmail.com>
2017-07-03 13:22:55 +0200

    setup ava for tests

---
+++ src/person/index.js
@@ -0,0 +1,4 @@
+
+import * as Actions from './actions'
+
+export default { Actions }
---
+++ test/person.js
@@ -0,0 +1,9 @@
+
+import test from 'ava'
+import Person from '../src/person'
+
+console.log(Person)
+
+test(t => {
+  t.pass()
+})
